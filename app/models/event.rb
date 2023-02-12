2c4142f366c33e9c7ff877038be2d64286dce5f8
AyakaOoshima <och1215bi3@gmail.com>
2023-02-12 15:16:00 +0900

    feat: バリデーション作成

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,24 @@
 class Event < ApplicationRecord
+  validates :title, presence: true, length: { maximum: 40 }
+  validates :content, presence: true, length: { maximum: 1000 }
+  validates :date, presence: true
+  validates :start_time, presence: true
+  validates :end_time, presence: true
+  validates :place, presence: true, length: { maximum: 40 }
+  # TODO：画像アップロード機能実装後
+  # validates :image, presence: true
+  validate :date_cannot_be_in_the_past, on: :create
+  validate :start_end_check
+
+  def date_cannot_be_in_the_past
+    if date.present? && date < Date.today
+      errors.add(:expiration_date, "過去の日付は入力できません")
+    end
+  end
+
+  def start_end_check
+    if start_time > end_time
+      errors.add(:end_time, "は開始時刻より遅い時間を選択してください")
+    end
+  end
 end
