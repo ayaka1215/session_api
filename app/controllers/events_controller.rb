ea034fd3220c818acfa7967aa3c8be2edcb74ddd
AyakaOoshima <och1215bi3@gmail.com>
2023-02-18 09:35:44 +0900

    feat: destroyアクション、set_target_eventアクション設定

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,5 @@
 class EventsController < ApplicationController
+    before_action :set_target_event, only: %i[show update destroy]
     def index
 		events = Event.all.order(date: "ASC")
         render json: events
@@ -10,18 +11,25 @@ class EventsController < ApplicationController
     end
 
     def show
-        event = Event.find(params[:id])
         render json: event
     end
 
     def update
-        event = Event.find(params[:id])
         event.update!(event_params)
+        head :ok
+    end
+
+    def destroy
+        event.destroy
         head :ok
     end
 
     private
     def event_params
         params.require(:event).permit(:title, :content, :date, :start_time, :end_time, :place, :image)
+    end
+
+    def set_target_event
+        event = Event.find(params[:id])
     end
 end
