4ad61848ef9f7836f391bde4543e8fb6992839cd
AyakaOoshima <och1215bi3@gmail.com>
2023-03-01 14:57:38 +0900

    feat: 認証関連追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'auth'
   resources :events
+  mount_devise_token_auth_for 'User', at: 'auth', controllers: {
+    registrations: 'auth/registrations'
+  }
+  namespace :auth do
+    resources :sessions, only: %i[index]
+  end
 end
