595c38c390981bc9a463a4b855fa93d3ad01fe58
AyakaOoshima <och1215bi3@gmail.com>
2023-03-07 22:57:33 +0900

    fix: サインアップエラー解消

--- app/controllers/auth/registrations_controller.rb
+++ app/controllers/auth/registrations_controller.rb
@@ -2,6 +2,6 @@ class Auth::RegistrationsController < DeviseTokenAuth::RegistrationsController
   private
 
   def sign_up_params
-    params.permit(:email, :password, :password_confirmation, :name)
+    params.require(:registration).permit(:email, :password, :password_confirmation, :name)
   end
 end
