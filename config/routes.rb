c0676a606589f9dada7f87e04c762be8cf7746e7
AyakaOoshima <och1215bi3@gmail.com>
2023-03-12 20:35:57 +0900

    feat: パート一覧取得

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :events
   resources :users
+  resources :parts
   mount_devise_token_auth_for 'User', at: 'auth', controllers: {
     registrations: 'auth/registrations'
   }
