f2f7c4f1a2d42f85fac844a30efcf6790f88540d
AyakaOoshima <och1215bi3@gmail.com>
2023-03-12 18:29:13 +0900

    fix: アソシエーション不具合修正

--- app/models/part.rb
+++ app/models/part.rb
@@ -1,4 +1,4 @@
 class Part < ApplicationRecord
   has_many :user_part_relations, dependent: :delete_all
-  has_many :users. through: :user_part_relations
+  has_many :users, through: :user_part_relations
 end
