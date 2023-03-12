b5218f2426ce26d26c28c37a9ec6594ba8e6fb08
AyakaOoshima <och1215bi3@gmail.com>
2023-03-12 12:10:07 +0900

    feat: 画像アップロード設定
    :q

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_12_022441) do
+ActiveRecord::Schema.define(version: 2023_03_12_023116) do
 
   create_table "events", charset: "utf8mb4", force: :cascade do |t|
     t.string "title"
